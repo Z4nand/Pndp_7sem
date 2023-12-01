fcffdde391f6484b2844054cbd8a71d62abdd2dd
zanand <zakharchenko.tolik@list.ru>
2023-12-02 01:06:48 +0600

    lab5

---
+++ lab5/src/Main.java
@@ -0,0 +1,112 @@
+
+
+//Лабораторная работа 5
+//Поисковый робот с Executor Service
+//В отличии от программы из задания 4 количество потоков должно быть ограниченным. Следует использовать один из
+//вариантов Java Executor Service. Формирование единого индекса должно осуществляться с использованием Future.
+//Все объекты Future необходимо собрать в список и только когда последнее "задание" на обработку будет выдано
+//начинать считывать результаты (метод get())
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        long startTime = System.currentTimeMillis();
+        List<String> listJavaFile = searchJavaFiles("/home/zanand/Study/JavaFolder/pndp/spring-framework");
+        printer(clsHandler(listJavaFile));
+        // Фиксируем конечное время выполнения программы
+        long endTime = System.currentTimeMillis();
+        System.out.println("Время выполнения программы: " + (endTime - startTime) + " миллисекунд");
+    }
+
+    // 1 Поиск Java файлов
+    public static List<String> searchJavaFiles(String directoryPath) throws IOException {
+        try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
+            return paths.parallel()
+                    .filter(Files::isRegularFile)
+                    .filter(p -> p.toString().endsWith(".java"))
+                    .map(Path::toString)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    // 2 Чтение
+    public static String readFromFile(String filePath) throws IOException {
+        String content;
+        content = Files.readString(Path.of(filePath));
+        return content;
+    }
+
+    // 3 Обработка
+    public static Map<String, List<String>> clsHandler(List<String> listJavaFile) throws IOException, InterruptedException {
+        Map<String, List<String>> parent_children = new HashMap<>(); // создаем map для хранения родителя и его детей
+        Pattern pattern_full = Pattern.compile("(\\binterface\\s\\w+\\s\\bextends\\s[\\w, ]+\\b)|(\\bclass\\s\\w+\\s\\bextends\\s\\w+\\b)");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(8); // Создаем ExecutorService с ограниченным количеством потоков
+        List<Future<Void>> futures = new ArrayList<>();
+
+
+        for (String i : listJavaFile) {
+            Future<Void> future = executorService.submit(() -> {
+                try {
+                    String text = readFromFile(i);
+                    Matcher matcher = pattern_full.matcher(text);
+                    while (matcher.find()) {
+                        String line = matcher.group();
+                        String[] parts = line.split("\\s");
+                        String className = parts[1];
+                        String parentName = parts[parts.length - 1];
+                        synchronized (parent_children) {
+                            List<String> childClasses = parent_children.getOrDefault(parentName, new ArrayList<>());
+                            childClasses.add(className);
+                            parent_children.put(parentName, childClasses);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            });
+            futures.add(future);
+        }
+
+        for (Future<Void> future : futures) {
+            try {
+                future.get(); // ждем завершения всех потоков
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        executorService.shutdown(); // завершаем ExecutorService
+
+        return parent_children;
+    }
+
+    // 4 print
+    public static void printer(Map<String, List<String>> parent_children) throws IOException {
+        for (Map.Entry<String, List<String>> entry : parent_children.entrySet()) {
+            String parentClass = entry.getKey();
+            List<String> childClasses = entry.getValue();
+            System.out.println(parentClass + ": " + childClasses.toString());
+        }
+    }
+}
+
+
+
+
